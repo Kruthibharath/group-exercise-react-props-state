dab03831af842b925705434753cd0b87265de62f
Kruthibharath <kruthi.bharath@gmail.com>
2023-11-30 13:35:46 +0000

    feat: passed jokes array as a props to the chuck_Joke component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import ChuckCard from "./components/chuck_card";
 import ChuckInfo from "./components/chuck_info";
+import ChuckJoke from "./components/chuck_joke";
 import Joke from "./joke";
 
 function App() {
@@ -38,6 +39,7 @@ function App() {
       <ChuckInfo whalesSaved={whalesSaved} roundHouseKicks={roundHouseKicks} />
 
       <h2>Jokes: </h2>
+      <ChuckJoke jokes={jokes} />
     </div>
   );
 }
--- src/components/chuck_joke.tsx
+++ src/components/chuck_joke.tsx
@@ -1,9 +1,19 @@
+import Joke from "../joke";
 interface ChuckJokeProps {
-
+  jokes: Joke[];
 }
 
-const ChuckJoke: React.FC<ChuckJokeProps> = () => <p></p>
-
-
+const ChuckJoke: React.FC<ChuckJokeProps> = ({ jokes }) => {
+  return (
+    <>
+      <div>
+        <h2>Chuck Jokes:</h2>
+        {jokes.map((joke) => (
+          <p key={joke.id}>{joke.joke}</p>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default ChuckJoke;
